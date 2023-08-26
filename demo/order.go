21396276d90c554570e74e42ddefb1304a5f64d2
iap-server-git <105693042+iap-server-git@users.noreply.github.com>
2023-08-26 18:24:25 +0800

    Update order.go
    The deprecated parameter accountFlag is deleted.

--- demo/order.go
+++ demo/order.go
@@ -14,67 +14,61 @@
  *    limitations under the License.
  *
  */
-package demo
+ package demo
 
-import (
-	"fmt"
-	"log"
-)
-
-type OrderClient struct {
-}
-
-var OrderDemo = &OrderClient{}
-
-func getOrderUrl(accountFlag int) string {
-	if accountFlag == 1 {
-		// site for telecom carrier
-		return "https://orders-at-dre.iap.cloud.huawei.eu"
-	} else {
-		// TODO: replace the (ip:port) to the real one
-    return  "http://exampleserver/_mockserver_"
-  }
-
-}
-
-func (orderDemo *OrderClient) VerifyToken(purchaseToken, productId string, accountFlag int) {
-	bodyMap := map[string]string{"purchaseToken": purchaseToken, "productId": productId}
-	url := getOrderUrl(accountFlag) + "/applications/purchases/tokens/verify"
-	bodyBytes, err := SendRequest(url, bodyMap)
-	if err != nil {
-		log.Printf("err is %s", err)
-	}
-	// TODO: display the response as string in console, you can replace it with your business logic.
-	log.Printf("%s", bodyBytes)
-}
-
-func (orderDemo *OrderClient) CancelledListPurchase(endAt int64, startAt int64, maxRows int, productType int, continuationToken string, accountFlag int) {
-	bodyMap := map[string]string{
-		"endAt":             fmt.Sprintf("%v", endAt),
-		"startAt":           fmt.Sprintf("%v", startAt),
-		"maxRows":           fmt.Sprintf("%v", maxRows),
-		"type":              fmt.Sprintf("%v", productType),
-		"continuationToken": continuationToken,
-	}
-	url := getOrderUrl(accountFlag) + "/applications/v2/purchases/cancelledList"
-	bodyBytes, err := SendRequest(url, bodyMap)
-	if err != nil {
-		log.Printf("err is %s", err)
-	}
-	// TODO: display the response as string in console, you can replace it with your business logic.
-	log.Printf("%s", bodyBytes)
-}
-
-func (orderDemo *OrderClient) ConfirmPurchase(purchaseToken, productId string, accountFlag int) {
-	bodyMap := map[string]string{
-		"purchaseToken": purchaseToken,
-		"productId":     productId,
-	}
-	url := getOrderUrl(accountFlag) + "/applications/v2/purchases/confirm"
-	bodyBytes, err := SendRequest(url, bodyMap)
-	if err != nil {
-		log.Printf("err is %s", err)
-	}
-	// TODO: display the response as string in console, you can replace it with your business logic.
-	log.Printf("%s", bodyBytes)
-}
+ import (
+	 "fmt"
+	 "log"
+ )
+ 
+ type OrderClient struct {
+ }
+ 
+ var OrderDemo = &OrderClient{}
+ 
+ func getOrderUrl() string {
+	 // TODO: replace the (ip:port) to the real one
+	 return  "http://exampleserver/_mockserver_"
+ }
+ 
+ func (orderDemo *OrderClient) VerifyToken(purchaseToken, productId string) {
+	 bodyMap := map[string]string{"purchaseToken": purchaseToken, "productId": productId}
+	 url := getOrderUrl() + "/applications/purchases/tokens/verify"
+	 bodyBytes, err := SendRequest(url, bodyMap)
+	 if err != nil {
+		 log.Printf("err is %s", err)
+	 }
+	 // TODO: display the response as string in console, you can replace it with your business logic.
+	 log.Printf("%s", bodyBytes)
+ }
+ 
+ func (orderDemo *OrderClient) CancelledListPurchase(endAt int64, startAt int64, maxRows int, productType int, continuationToken string) {
+	 bodyMap := map[string]string{
+		 "endAt":             fmt.Sprintf("%v", endAt),
+		 "startAt":           fmt.Sprintf("%v", startAt),
+		 "maxRows":           fmt.Sprintf("%v", maxRows),
+		 "type":              fmt.Sprintf("%v", productType),
+		 "continuationToken": continuationToken,
+	 }
+	 url := getOrderUrl() + "/applications/v2/purchases/cancelledList"
+	 bodyBytes, err := SendRequest(url, bodyMap)
+	 if err != nil {
+		 log.Printf("err is %s", err)
+	 }
+	 // TODO: display the response as string in console, you can replace it with your business logic.
+	 log.Printf("%s", bodyBytes)
+ }
+ 
+ func (orderDemo *OrderClient) ConfirmPurchase(purchaseToken, productId string) {
+	 bodyMap := map[string]string{
+		 "purchaseToken": purchaseToken,
+		 "productId":     productId,
+	 }
+	 url := getOrderUrl() + "/applications/v2/purchases/confirm"
+	 bodyBytes, err := SendRequest(url, bodyMap)
+	 if err != nil {
+		 log.Printf("err is %s", err)
+	 }
+	 // TODO: display the response as string in console, you can replace it with your business logic.
+	 log.Printf("%s", bodyBytes)
+ }
