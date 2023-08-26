7029d08aaa17100437fb373b11fde141c952e83c
iap-server-git <105693042+iap-server-git@users.noreply.github.com>
2023-08-26 17:30:11 +0800

    Update order_service.php
    The deprecated parameter accountFlag is deleted.

--- order_service.php
+++ order_service.php
@@ -21,21 +21,12 @@ class OrderService {
     // TODO: replace the (ip:port) to the real one, and if the protocol is https, you should deal with the license yourself.
     //yourself
     const TOC_SITE_URL = "https://ip:port";
-    //site fore telecom carrier
-    const TOBTOC_SITE_URL = "https://orders-at-dre.iap.cloud.huawei.eu";
 
     const VERIFY_TOKEN_URL = "/applications/purchases/tokens/verify";
     const CANCELLED_LIST_PURCHASE_URL = "/applications/v2/purchases/cancelledList";
     const CONFIRM_PURCHASE_URL = "/applications/v2/purchases/confirm";
 
-    public function getRootUrl($accountFlag) {
-        if ($accountFlag != null && $accountFlag == 1) {
-            return self::TOBTOC_SITE_URL;
-        }
-        return self::TOC_SITE_URL;
-    }
-
-    public function verifyToken($purchaseToken, $productId, $accountFlag) {
+    public function verifyToken($purchaseToken, $productId) {
         // fetch the App Level AccessToken
         $appAT = AtDemo::getAppAT();
         if ($appAT == null) {
@@ -48,13 +39,13 @@ class OrderService {
         $body = ["purchaseToken" => $purchaseToken, "productId" => $productId];
         $msgBody = json_encode($body);
 
-         $response = AtDemo::httpPost(self::getRootUrl($accountFlag).self::VERIFY_TOKEN_URL, $msgBody, 5, 5, $headers, true);
+         $response = AtDemo::httpPost(self::TOC_SITE_URL.self::VERIFY_TOKEN_URL, $msgBody, 5, 5, $headers, true);
 
         // TODO: display the response as string in console, you can replace it with your business logic.
         echo $response;
     }
 
-    public function cancelledListPurchase($endTime, $startTime, $maxRows, $type, $continuationToken, $accountFlag) {
+    public function cancelledListPurchase($endTime, $startTime, $maxRows, $type, $continuationToken) {
         // fetch the App Level AccessToken
         $appAT = AtDemo::getAppAT();
         if ($appAT == null) {
@@ -67,13 +58,13 @@ class OrderService {
         $body = ["endAt" => $endTime, "startAt" => $startTime, "maxRows" => $maxRows, "type" => $type, "continuationToken" => $continuationToken];
         $msgBody = json_encode($body);
 
-         $response = AtDemo::httpPost(self::getRootUrl($accountFlag).self::CANCELLED_LIST_PURCHASE_URL, $msgBody, 5, 5, $headers, true);
+         $response = AtDemo::httpPost(self::TOC_SITE_URL.self::CANCELLED_LIST_PURCHASE_URL, $msgBody, 5, 5, $headers, true);
 
         // TODO: display the response as string in console, you can replace it with your business logic.
         echo $response;
     }
 
-    public function confirmPurchase($purchaseToken, $productId, $accountFlag) {
+    public function confirmPurchase($purchaseToken, $productId) {
         // fetch the App Level AccessToken
         $appAT = AtDemo::getAppAT();
         if($appAT == null){
@@ -86,7 +77,7 @@ class OrderService {
         $body = ["purchaseToken"=>$purchaseToken,"productId"=>$productId];
         $msgBody = json_encode($body);
 
-        $response = AtDemo::httpPost(self::getRootUrl($accountFlag).self::CONFIRM_PURCHASE_URL, $msgBody, 5, 5, $headers, true);
+        $response = AtDemo::httpPost(self::TOC_SITE_URL.self::CONFIRM_PURCHASE_URL, $msgBody, 5, 5, $headers, true);
 
         // TODO: display the response as string in console, you can replace it with your business logic.
         echo $response;
